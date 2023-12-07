e480163d983347484fcc8373a50464e872089843
Sina-Haz <101819919+Sina-Haz@users.noreply.github.com>
2023-12-07 16:16:05 -0500

    slightly fixed weights

--- particle_filter.py
+++ particle_filter.py
@@ -1,17 +1,30 @@
 import argparse
 import numpy as np
 from create_scene import load_polygons
-from math import pi
+import math
 import scipy.stats
 from create_scene import create_plot,load_polygons
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 from diff_drive import Car
+from simulate import landmark_sensor
 
+"""
+For observation model: getting P(z|x), the std deviation of distance and angle measurements are independent
+ - First compute probability of getting the distance measurements by doing landmark sensor at particle and comparing it
+to the readings (these are all independent)
+- Compute probability of getting angular measurements by comparing landmark sensor to readings (again all independent)
+- Product of all of these is going to be P(z|x)
 
+ - To update the weights: get distance and direction (without any noise so don't use landmark sensor) for each landmark
+ - compare this to reading (which is the mean) and standard deviation
+
+Resampling algorithm: can use any of the 4 in the kalman-filter.ipynb reference
+"""
 # Global where we store the standard deviation of the controls according to the 'L'/'H' param and odometry model from 
 # readings.npy
 std_dev = None
+sensor_std_dev = 0.02
 
 # This will be 200 controls from readings.npy
 def load_sensed_controls(readings):
@@ -42,14 +55,6 @@ def set_std_dev(filename):
     else:
         return Exception
     
-def cov_matrix():
-    global std_dev
-    variances = std_dev**2
-    cov = np.eye(2,2)
-    cov[0,0]*=variances[0]
-    cov[1,1]*=variances[1]
-    return cov
-    
 # New version of next that can be applied to all particles and noise samples at once
 def next_np(q,u,dt=0.1):
     dq = np.zeros_like(q)
@@ -59,33 +64,68 @@ def next_np(q,u,dt=0.1):
     return q + (dq * dt)
     
 # Move particles according to control input u, with Gaussian noise Q defined by our odometry model 
-def predict(particles, u, N):
+def prediction(particles, u, N):
     global std_dev
     noise_samples = np.random.normal(loc=u, scale=std_dev,size=(N,2))
     return next_np(particles,noise_samples)
-        
 
-#Creates uniform particles by randomly sampling possible robot configurations
-def create_uniform_particles(x_range, y_range, N):
-    particles = np.empty((N, 2))
-    particles[:, 0] = np.random.uniform(x_range[0], x_range[1], size=N)
-    particles[:, 1] = np.random.uniform(y_range[0], y_range[1], size=N)
-    return particles
+def get_landmark_dist(particle, landmark):
+    diff = particle[:2] - landmark
 
-def find_next_position(particles, control, dt=.1):
-    N = len(particles)
-    dist = (control[0] * dt)
-    particles[:, 0] += np.cos(control[1]) * dist
-    particles[:, 1] += np.sin(control[1]) * dist
+    # Rotate relative position based on robot's orientation
+    rotated_x = diff[0] * np.cos(-particle[2]) - diff[1] * np.sin(-particle[2])
+    rotated_y = diff[0] * np.sin(-particle[2]) + diff[1] * np.cos(-particle[2])
 
-def update_particles(particles, weights, distances, R, landmarks):
-    for i in range(len(landmarks)):
-        distance=np.power((particles[:,0] - landmarks[i][0])**2 +(particles[:,1] - landmarks[i][1])**2,0.5)
-        weights *= scipy.stats.norm(distance, R).pdf(distances[i][0])
- 
- 
-    weights += 1.e-300 # avoid round-off to zero
-    weights /= sum(weights)
+    # Calculate distance and angle to the landmark
+    distance = np.sqrt(rotated_x**2 + rotated_y**2)
+    angle = math.atan2(rotated_y, rotated_x)
+    return distance,angle
+
+#Returns the distance and the angle from every landmark in the scene to the ground truths of the rigid body.
+def landmark_dist(x, y, theta, landmark):
+    dx = landmark[0] - x
+    dy = landmark[1] - y
+
+    # Rotate relative position based on robot's orientation
+    rotated_x = dx * math.cos(-theta) - dy * math.sin(-theta)
+    rotated_y = dx * math.sin(-theta) + dy * math.cos(-theta)
+
+    # Calculate distance and angle to the landmark
+    distance = math.sqrt(rotated_x**2 + rotated_y**2)
+    angle = math.atan2(rotated_y, rotated_x)
+    return distance,angle
+
+
+# Don't need these for this file
+# #Creates uniform particles by randomly sampling possible robot configurations
+# def create_uniform_particles(x_range, y_range, N):
+#     particles = np.empty((N, 2))
+#     particles[:, 0] = np.random.uniform(x_range[0], x_range[1], size=N)
+#     particles[:, 1] = np.random.uniform(y_range[0], y_range[1], size=N)
+#     return particles
+
+# def find_next_position(particles, control, dt=.1):
+#     N = len(particles)
+#     dist = (control[0] * dt)
+#     particles[:, 0] += np.cos(control[1]) * dist
+#     particles[:, 1] += np.sin(control[1]) * dist
+
+def correction(particles, weights, reading, landmarks):
+    global sensor_std_dev
+    for i,l in enumerate(landmarks):
+        dist_distribution = scipy.stats.norm(loc = reading[i][0], scale=sensor_std_dev)
+        angle_distribution = scipy.stats.norm(loc = reading[i][1], scale=sensor_std_dev)
+        p_dists = np.array([landmark_dist(p[0],p[1],p[2],l) for p in particles])
+        probs_d = dist_distribution.pdf(p_dists[:,0])
+        probs_d /= np.sum(probs_d)
+        probs_a = angle_distribution.pdf(p_dists[:,1])
+        probs_a /= np.sum(probs_a)
+        weights *= probs_d*probs_a
+    weights += 1.e-300      # avoid round-off to zero
+    weights /= sum(weights) # normalize weights
+
+
+
 
 def systematic_resample(weights):
     N = len(weights)
@@ -149,8 +189,11 @@ if __name__ == '__main__':
     initPose = readings[0]
     particles = init_particles(initPose,N)
     weights = np.array([1.0]*N)
-    print(cov_matrix())
-    #particle_filter(contr, dists, landmarks, N)
+
+    for i in range(5):
+        particles = prediction(particles,contr[i],N)
+        correction(particles,weights,dists[i],landmarks)
+        print(weights)
 
 
 
